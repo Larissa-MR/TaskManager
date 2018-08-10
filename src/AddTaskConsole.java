8634ee6c87f53e89438b45d52ec00d26a5d80ba1
moonwatah <39312556+moonwatah@users.noreply.github.com>
2018-08-10 14:46:06 +0200

    Add files via upload

---
+++ src/AddTaskConsole.java
@@ -0,0 +1,53 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class AddTaskConsole {
+
+	public static Task taskQuery() throws ParseException {
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("\n\n\n______Add task______");
+		String titleInp = "";
+		while (titleInp.equals("")) {
+			System.out.println("Title: ");
+			titleInp = keyboard.nextLine();
+		}
+
+		String descriptionInp = "";
+		while (descriptionInp.equals("")) {
+			System.out.println("Description: ");
+			descriptionInp = keyboard.nextLine();
+		}
+
+		String dueDateInp = "";
+		Date d = null;
+		while (dueDateInp.equals("") || !SystemManager.dateIsValid(dueDateInp)) {
+			System.out.println("Due date (dd.mm.yyyy): ");
+			dueDateInp = keyboard.nextLine();
+		}
+		LocalDate dueDate = SystemManager.convertStringToLocalDate(dueDateInp);
+
+		String hoursInp = "";
+		int hours = 0;
+		do {
+			System.out.println("Time exposure (in h): ");
+			hoursInp = keyboard.nextLine();
+		} while (hoursInp.equals("") || SystemManager.StringToInt(hoursInp) == -1);
+		hours = SystemManager.StringToInt(hoursInp);
+
+		String priorityInp = "";
+		int priority = 0;
+		do {
+			System.out.println("Priority (1-low to 3-high): ");
+			priorityInp = keyboard.nextLine();
+		} while (SystemManager.priorityIsValid(priorityInp) == 0);
+		priority=SystemManager.StringToInt(priorityInp);
+		
+		System.out.println("Your task was successfully added!");
+		return new Task(titleInp, descriptionInp, dueDate, hours, priority);
+
+	}
+}
---
+++ src/SystemManager.java
@@ -0,0 +1,161 @@
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class SystemManager {
+
+	/**
+	 * 
+	 * 
+	 * @param inpUser user input for 'username'
+	 * @param inpPass user input for 'password'
+	 * @return the file's name in which both username and password were found
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@SuppressWarnings("resource")
+	public static String loginIsValid(String inpUser, String inpPass) throws ClassNotFoundException, IOException {
+		File directory = new File("C:\\Users\\Administrator\\Desktop\\TaskManager\\Employees_LoginData");
+		File[] files = directory.listFiles();
+		int i = 0;
+		if (inpUser.equals("") || inpPass.equals(""))
+			return null;
+		for (File f : files) {
+			Scanner fScanner = new Scanner(files[i]);
+			i++;
+			while (fScanner.hasNextLine()) {
+				String nextToken = fScanner.next();
+				if (nextToken.equals(inpUser)) {
+					nextToken = fScanner.next();
+					if (nextToken.equals(inpPass)) {
+						return f.getName();
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	
+	
+	
+	/**
+	 * return Employee for the given fileName
+	 * @param fileName
+	 * @return Employee that is serialized in that File
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	public static Employee getEmployeeByFileName(String fileName) throws ClassNotFoundException, IOException {
+		File directory = new File("C:\\Users\\Administrator\\Desktop\\TaskManager\\Employees");
+		File[] files = directory.listFiles();
+		int i = 0;
+		for (File f : files) {
+			if (f.getName().equals(fileName)) {
+				try {
+					FileInputStream fis = new FileInputStream(f);
+					ObjectInputStream ois = new ObjectInputStream(fis);
+					Object obj = ois.readObject();
+					return (Employee) obj;
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+					return null;
+				}
+
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	/**
+	 * readsFile and returns Object that is serialized in file, if object empty: return null
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static Object readFile(File file) throws IOException, ClassNotFoundException {
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Object obj = ois.readObject();
+			return obj;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	
+	
+	
+	
+	
+	
+	public static boolean dateIsValid(String dateInput) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		Date testDate = null;
+
+		try {
+			testDate = sdf.parse(dateInput);
+		}
+
+		catch (ParseException e) {
+			System.out.println("Wrong date format");
+			return false;
+		}
+
+		if (!sdf.format(testDate).equals(dateInput)) {
+			System.out.println("Wrong date format");
+			return false;
+		}
+
+		return true;
+	}
+	
+	
+
+	public static LocalDate convertStringToLocalDate(String dateString) throws ParseException {
+//		DateFormat formatter = new SimpleDateFormat("dd.mm.yyyy");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+		LocalDate localDate = LocalDate.parse(dateString, formatter);
+		return localDate;
+	}
+
+	public static int priorityIsValid(String priorityInp) {
+		if (priorityInp.equals("1") || priorityInp.equals("2") || priorityInp.equals("3")) {
+			return Integer.parseInt(priorityInp);
+		}
+		return 0;
+	}
+
+	public static int StringToInt(String inp) {
+		try {
+			return Integer.parseInt(inp);
+		} catch (Exception notInteger) {
+			return -1;
+		}
+
+	}
+
+
+	public static void taskSummaryOutput(Task task) {
+		System.out.println("Title: " + task.getTitle());
+		System.out.println("Description: " + task.getDescription());
+		System.out.println("Due date: " + task.getDueDate().toString());
+		System.out.println("Time exposure: " + task.getHours()+ " hours");
+		System.out.println("Priority: " + task.getPriority());
+
+	}
+	
+
+
+	
+}
